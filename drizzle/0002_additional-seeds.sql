38714166a40420aea64c3d2e420438c77f7b768f
Mohammad Jafari <jafarim@gmail.com>
2025-05-10 14:46:19 -0700

    add more seeds

---
+++ drizzle/0002_additional-seeds.sql
@@ -0,0 +1,13 @@
+INSERT INTO codes VALUES 
+  (6, 5, 'HIV', 'HIV/AIDS information sensitivity', 'sensitivity'),
+  (7, 5, 'SDV', 'Sexual assault, abuse, or domestic violence information sensitivity', 'sensitivity'),  
+  -- Refined substance use categories
+  (8, 5, 'OPIOIDUD', 'Opioid use disorder information sensitivity', 'sensitivity'),
+  (9, 5, 'ETH', 'Substance abuse treatment information sensitivity', 'sensitivity'),
+  -- Refined behavioral health categories
+  (10, 5, 'PSY', 'Psychiatry information sensitivity', 'sensitivity'),
+  (11, 5, 'PSYTHPN', 'Psychotherapy note information sensitivity', 'sensitivity'),
+  (12, 5, 'BHDS', 'Behavioral and developmental disability information sensitivity', 'sensitivity')
+ON CONFLICT ("id") DO NOTHING;
+
+
